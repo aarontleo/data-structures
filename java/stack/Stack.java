df9f473904a61c146388163b0aeecf3ab7ace09c
Aaron <aaron.leo14@gmail.com>
2020-06-02 20:15:37 -0400

    fixed functionality

--- java/stack/Stack.java
+++ java/stack/Stack.java
@@ -6,6 +6,9 @@ Operations on a Stack
 4. isFull - check to see if the stack is full
 5. isEmpty - check to see if the stack is empty
 */
+
+import java.util.Arrays;
+
 public class Stack {
     int top;
     int capacity;
@@ -18,19 +21,38 @@ public class Stack {
     }
 
     public void push(int element) {
-
+        if (isFull()) {
+            System.out.println("Stack Full!");
+        } else {
+            stackArray[++top] = element;
+            System.out.println("Pushed Element: " + element);
+        }
     }
 
-    public void pop() {
-        
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Empty!");
+        }
+        System.out.println("Popped Element: " + stackArray[top]);
+        return stackArray[top--];
     }
 
     public boolean isFull() {
         return top == capacity - 1 ? true : false;
     }
 
-    public void isEmpty() {
-        top == -1;
+    public boolean isEmpty() {
+        return top == -1 ? true : false;
+    }
+
+    public void display() {
+        System.out.println("----------------");
+        if (isEmpty()) {
+            System.out.println("Stack Empty!");
+        } else {
+            System.out.println(Arrays.toString(stackArray));
+        }
+        
     }
 
 }
--- java/stack/StackDriver.java
+++ java/stack/StackDriver.java
@@ -1,6 +1,13 @@
 public class StackDriver {
     public static void main(String[] args) {
         Stack s = new Stack(10);
+        s.push(1);
+        s.push(3);
+        s.push(5);
+        s.push(7);
+        s.pop();
+        s.push(9);
+        s.display();
 
     }
 }
